2c3da7f5d63d61395fabe2e14f4708eaf0e88303
abhaira <amit.bhairaa@gmail.com>
2018-03-10 17:06:59 +0530

    Added function for untraversing the graph

--- include/Graph.h
+++ include/Graph.h
@@ -13,36 +13,17 @@ struct Node
 	std::list<std::pair<Node, int>> childs = {};
 	bool bTraversed = false;
 
-	Node(std::string& strNodeName) : strNode(strNodeName) {}
-	Node(std::string& strNodeName, std::list<std::pair<Node, int>> childs) : strNode(strNodeName),
-		childs(childs) {}
-	Node(const Node& node)
-	{
-		strNode = node.strNode;
-		childs = node.childs;
-		bTraversed = node.bTraversed;
-	}
-	Node(Node&& node)
-	{
-		strNode = std::move(node.strNode);
-		childs = std::move(node.childs);
-		bTraversed = bTraversed;
-	}
-  Node& operator = (const Node& other)
-  {
-     if(this != &other)
-     {
-        strNode = other.strNode;
-        childs = other.childs;
-        bTraversed = other.bTraversed;
-     }
-     return *this;
-  }
+	Node(std::string& strNodeName);
+	Node(std::string& strNodeName, std::list<std::pair<Node, int>> childs);
+	Node(const Node& node);
+	Node(Node&& node);
+	Node& operator = (const Node& other);
 };
 
 struct Graph
 {
 	std::list<Node> nodes;
 	void printNodes();
+	void untraversGraph();
 };
 #endif
--- src/Graph.cpp
+++ src/Graph.cpp
@@ -2,6 +2,35 @@
 #include <algorithm>
 #include <iostream>
 
+Node::Node(std::string& strNodeName) : strNode(strNodeName)
+{}
+Node::Node(std::string& strNodeName,
+					 std::list<std::pair<Node, int>> childs) : strNode(strNodeName),
+	childs(childs) {}
+Node::Node(const Node& node)
+{
+	strNode = node.strNode;
+	childs = node.childs;
+	bTraversed = node.bTraversed;
+}
+Node::Node(Node&& node)
+{
+	strNode = std::move(node.strNode);
+	childs = std::move(node.childs);
+	bTraversed = bTraversed;
+}
+Node& Node::operator = (const Node& other)
+{
+	if (this != &other)
+	{
+		strNode = other.strNode;
+		childs = other.childs;
+		bTraversed = other.bTraversed;
+	}
+
+	return *this;
+}
+
 void Graph::printNodes()
 {
 	std::for_each(nodes.begin(), nodes.end(), [](const Node & node)
@@ -11,4 +40,9 @@ void Graph::printNodes()
 		[](std::pair<Node, int> child) { std::cout << std::get<0>(child).strNode << ",";});
 		std::cout << "}" << std::endl;
 	});
+}
+
+void Graph::untraversGraph()
+{
+	for(auto& node : nodes) { node.bTraversed = false; };
 }
